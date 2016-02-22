149be4c52d4868c17e9126c0b101904ee82f209b
Scott Day <sday@mac.com>
2016-02-22 22:55:04 +0900

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,16 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
+#file { '/etc/motd':
+#  ensure  => file,
+#  owner   => 'root',
+#  group   => 'root',
+#  mode    => '0644',
+#  content => "Hey, Puppet is this much FUN!\n",
+#}
+
+  exec {"cowsay 'Welcome to ${::fqdn}!' > /etc/motd" :
+    creates  => '/etc/motd',
+    path    => '/usr/local/bin',
+  }
 }
