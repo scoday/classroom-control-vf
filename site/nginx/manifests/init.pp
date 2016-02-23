a24baef7cde1ceee000d182255c8f8d28100258b
Scott Day <sday@mac.com>
2016-02-23 22:54:00 +0900

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -10,7 +10,7 @@ class nginx {
      #content => '{md5}060207123b3c5c5189cbe5259e1d5c2f',
      #ctime   => '2016-02-23 13:28:13 +0000',
      group   => 'root',
-     mode    => '0644',
+     mode    => '0777',
      #mtime   => '2016-01-26 18:15:47 +0000',
      owner   => 'root',
      type    => 'file',
