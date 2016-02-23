f9c23ff0a44eaaa26013ff654c4eb6d16489940b
Scott Day <sday@mac.com>
2016-02-23 22:59:21 +0900

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -6,6 +6,7 @@ class nginx {
 
    file { '/etc/nginx/nginx.conf':
      ensure  => 'file',
+     source  => 'puppet:///modules/nginx/nginx.conf',
      #content => '{md5}060207123b3c5c5189cbe5259e1d5c2f',
      #ctime   => '2016-02-23 13:28:13 +0000',
      group   => 'root',
