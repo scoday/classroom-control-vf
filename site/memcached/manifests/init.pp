5d5eb501e67e5ebf0b00a1d9125935a3bb4411d7
Scott Day <sday@mac.com>
2016-02-23 22:03:49 +0900

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,40 +1,42 @@
 # HERE IS MEMCACHED
-#class memcached {
-#  file { '/etc/sysconfig/memcached.conf':
-#    ensure => file,
-#    owner  => 'root',
-#    group  => 'root',
-#    mode   => '0644',
-#    source => 'puppet:///modules/memcached/sysconfig/memcached.conf',
-#    notify => Service['memcached'],
-#  }
-#
-#  service { 'memcached':
-#    ensure => running,
-#    enable => true,
-#  }
-#
-#  package { 'memcached':
-#    ensure => present,
-#    before => File['/etc/sysconfig/memcached.conf'],
-#  }
-#}
-
 class memcached {
-  package{ 'memcached':
-    ensure  => present,
-    before  => File['/etc/sysconfig/memcached'],
-  }
-  
-  file{ '/etc/sysconfig/memcached':
-    ensure  =>  'file',
-    source  => 'puppet:///modules/memcached/sysconfig/memcached',
-    require => Package['memcached'],
-    notify  => Service['memcached'],
+  file { '/etc/sysconfig/memcached.conf':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/memcached/sysconfig/memcached.conf',
+    notify => Service['memcached'],
   }
 
-  service{ 'memcached':
-    ensure    => 'running',
-    subscribe =>  File['/etc/sysconfig/memcached'],
+  service { 'memcached':
+    ensure => running,
+    enable => true,
+  }
+
+  package { 'memcached':
+    ensure => present,
+    before => File['/etc/sysconfig/memcached.conf'],
   }
 }
+
+
+### CLEANER ? 
+#class memcached {
+#  package{ 'memcached':
+#    ensure  => present,
+#    before  => File['/etc/sysconfig/memcached'],
+#  }
+  #
+  #file{ '/etc/sysconfig/memcached':
+ #   ensure  =>  'file',
+ #   source  => 'puppet:///modules/memcached/sysconfig/memcached',
+ #   require => Package['memcached'],
+ #   notify  => Service['memcached'],
+#  }
+#
+#  service{ 'memcached':
+#    ensure    => 'running',
+#    subscribe =>  File['/etc/sysconfig/memcached'],
+#  }
+#}
