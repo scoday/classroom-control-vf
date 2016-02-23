845149e83fa36bf4d7946b16bfd97cf3a2647719
Scott Day <sday@mac.com>
2016-02-23 21:48:19 +0900

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,20 @@
+# HERE IS MEMCACHED
+class memcached {
+  file { '/etc/sysconfig/memcached.conf':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/memcached/memcached.conf',
+    notify => Service['memcached'],
+  }
+
+  service { 'memcached':
+    ensure => running,
+    enable => true,
+  }
+
+  package { 'memcached':
+    ensure => present,
+    before => File['/etc/sysconfig/memcached.conf'],
+}
