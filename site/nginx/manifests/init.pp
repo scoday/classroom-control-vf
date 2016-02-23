329bb2cee317988b640d35221482d5dda6985087
Scott Day <sday@mac.com>
2016-02-23 22:26:55 +0900

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,10 @@
+class nginx {
 
+   package { 'nginx':
+   ensure => present,
+   #before => File['/etc/sysconfig/memcached.conf'],
+   }
+
+ }
+
+ ## Add the package first and then use puppet resource file /etc/nginx/nginx.conf ##
