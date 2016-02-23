d5071722edb22b76720e50a3b899403d7c410d73
Scott Day <sday@mac.com>
2016-02-23 18:58:50 +0900

    update to comment out the other crap

--- manifests/site.pp
+++ manifests/site.pp
@@ -55,11 +55,11 @@ node default {
   #   creates  => '/etc/motd',
   #    path    => '/usr/local/bin',
   # }
-  host { 'testing.puppetlabs.vm':
-  ensure => 'present',
-  ip     => '127.0.0.1',
-  target => '/etc/hosts',
-}
-}
+# host { 'testing.puppetlabs.vm':
+#  ensure => 'present',
+#  ip     => '127.0.0.1',
+#  target => '/etc/hosts',
+#}
+#}
   include users
 }
